40e18847c9f316771276dd83a8775018962db15b
Adelino Afonso <aafavelino@icloud.com>
2018-02-07 09:45:30 -0300

    Iniciando canais virtuais

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,22 @@ using namespace std;
 int sc_main (int argc, char* argv[]) {
 	REDE *teste = new REDE("rede");
 
+			/*Ver este caso depois...*/		
+		// teste->injeta_flits(0,6, 6, 6);
+		// teste->injeta_flits(1,6, 6, 6);
+		// teste->injeta_flits(2,6, 6, 6);
+		// teste->injeta_flits(3,6, 6, 6);
+		// teste->injeta_flits(4,6, 6, 6);
+		// teste->injeta_flits(5,6, 6, 6);
 
-		// teste->injeta_flits(2,0, 0, 0);
-		
-		teste->injeta_flits(0,6, 6, 6);
-		teste->injeta_flits(1,6, 6, 6);
-		teste->injeta_flits(2,6, 6, 6);
-		teste->injeta_flits(3,6, 6, 6);
-		teste->injeta_flits(4,6, 6, 6);
+
 		teste->injeta_flits(5,6, 6, 6);
+		teste->injeta_flits(5,0, 6, 6);
+		teste->injeta_flits(0,0, 6, 6);
+		teste->injeta_flits(6,6, 0, 0);
 
 
-		// teste->injeta_flits(6,0, 6, 6);
-		// teste->injeta_flits(6,1, 6, 6);
-		// teste->injeta_flits(6,2, 6, 6);
-		// teste->injeta_flits(6,3, 6, 6);
-		// teste->injeta_flits(6,4, 6, 6);
+
 		 sc_start();	
 
 			
